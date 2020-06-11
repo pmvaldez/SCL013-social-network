ea5e02ea8ddcaff74dd76bc0f453b3a469980a0e
pmvaldez <valdezp69@gmail.com>
2020-06-10 21:24:28 -0400

    Css de login y registro ppal

--- src/vistas/login.js
+++ src/vistas/login.js
@@ -2,22 +2,31 @@ import { funLoginUsuario } from '../lib/controladorFirebase.js';
 
 export const ingresarUsuario = () => {
   const loginEmprendedora = ` 
-    <div class='container'>
-      <div class='login-container'>
-        <div class='register'>
-          <h2>Iniciar Sesión</h2>
-            <form action=''>
-              <input type='text' id='correoLogin' placeholder='Correo electrónico' class='correo'>
-              <input type='password' id='contrasenaLogin' placeholder='Ingrese su contraseña' class='pass'>
-              <button id='login' class='submit' >Iniciar sesión</button> <br> <br>
-              <a href='#/olvidocontraseña'>¿Olvidaste tu contraseña?</a> <br>
-              <a>¿No tienes cuenta? </a><a href='#/registro'>Regístrate</a>
-            </form>
-          <h1 id='mensajeLogin'></h1>
-        </div>
-      </div>
+<div class="banner">
+  <img src="./img/WomenBanner.png" alt="logo" class="imgLogo">
+</div>
+<div class="laptopPic"></div>
+<div class="container">
+  <div class="login-container">
+    <div class="login">
+      <h2>Iniciar Sesión</h2>
+      <form>
+        <input type="text" id="correoLogin" placeholder="Correo electrónico" class="correo">
+        <input type="password" id="contrasenaLogin" placeholder="Ingrese su contraseña" class="correo">
+       </form>
+      <button type ="button" id="login" class="submit" >Iniciar sesión</button>
+      <button id="loginFace" class="fb"><i class="fab fa-facebook-f"></i> Acceder con Facebook</button>
+      <button id="loginGoogle" class="goo"><i class="fab fa-google"></i> Acceder con Google</button>
+      <a href='#/olvidocontraseña'>¿Olvidaste tu contraseña?</a> <br>
+      <a>¿No tienes cuenta? </a><a href='#/registro'>Regístrate</a>
+      <h1 id='mensajeLogin'></h1>
     </div>
-  `;
+  </div>
+</div>
+<div class="footer">
+  <p>Copyright by Marcela / Paula & Yosliana / 2020 / Laboratoria013</p>
+</div> 
+`;
   const divElemen = document.createElement('div');
   divElemen.innerHTML = loginEmprendedora;
   const botonLogin = divElemen.querySelector('#login');
--- src/vistas/registro.js
+++ src/vistas/registro.js
@@ -2,27 +2,32 @@ import { funRegistroUsuario, funLoginGoogle, funLoginFacebook } from '../lib/con
 
 export const registroUsuario = () => {
   const registroEmprendedora = `
-<div class="container">
-<div class="login-container">
-  <div class="register">
-    <h2>Registrate</h2>
-    <form action="">
-      <input type="text" id="correoRegistro" placeholder="Correo electrónico" class="correo">
-      <input type="password" id="contrasenaRegistro" placeholder="Ingrese su contraseña" class="pass">
-      <button id="registro" class="submit" >Regístrarte</button><br>
-      <a>¿Tienes una cuenta? </a><a href='#/login'>Inicia sesión</a><br><br>
-           </form>
+<div class="banner">
+ <img src="./img/WomenBanner.png" alt="logo" class="imgLogo">
+</div>
+<div class="laptopPic"></div>
+ <div class="container">
+   <div class="login-container">
+     <div class="register">
+       <h2>Registrate</h2>
+       <form>
+         <input type="text" id="correoRegistro" placeholder="Correo electrónico" class="correo">
+         <input type="password" id="contrasenaRegistro" placeholder="Ingrese su contraseña" class="correo">
+       </form>
+       <button type ="button" id="registro" class="submit" >Regístrarte</button>
+       <button id="loginFace" class="fb"><i class="fab fa-facebook-f"></i> Acceder con Facebook</button>
+       <button id="loginGoogle" class="goo"><i class="fab fa-google"></i> Acceder con Google</button>
+       <a>¿Tienes una cuenta? </a><a href='#/login'>Inicia sesión</a><br><br>
        <h1 id='mensajeRegistro'></h1>
-  </div>
-  <div class="login">
-    <h2>Regístrate con</h2>
-    <div class="login-items">
-      <button id="loginFace" class="fb"><i class="fab fa-facebook-f"></i> Acceder con Facebook</button>
-      <button id="loginGoogle" class="goo"><i class="fab fa-google"></i> Acceder con Google</button>
-      </div>
+      </div>   
+    </div>
   </div>
 </div>
-</div>`;
+<div class="footer">
+ <p>Copyright by Marcela / Paula & Yosliana / 2020 / Laboratoria013</p>
+</div> `;
+
+
   const divElemen = document.createElement('div');
   divElemen.innerHTML = registroEmprendedora;
   const btnGoogle = divElemen.querySelector('#loginGoogle');
