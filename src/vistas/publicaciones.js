a93ea56110c4bc6c93eb6932880dae13cb515fb1
marce-8888 <mtancarao@outlook.cl>
2020-06-15 08:49:37 -0400

    muro y css

--- src/vistas/publicaciones.js
+++ src/vistas/publicaciones.js
@@ -1,36 +1,55 @@
-import { cerrarSesion } from '../lib/controladorFirebase.js';
-
 export const publicacionUsuario = () => {
   const muroEmprendedora = `
-
-  <div class="banner">
- <img src="./img/WomenBanner.png" alt="logo" class="imgLogo">
+  <div class="father">
+  <header class="header">
+<div class="banner">
+  <img src="./img/WomenBanner.png" alt="logo" class="imgLogo">
 </div>
-<br><br><br><br><br><br>
-<h1>Bienvenida a Women Today</h1>
-
-<button id="cerrar">Cerrar sesión</button>
-<textarea placeholder="Publica tu Comentario" id="comentario" cols="30" rows="10"></textarea>
-<input class='correo' type="file" id="imagen">
-<img id="foto"/>
-<button class='submit' id="cargarImagen">Cargar Imagen</button>
-<h1 id='mensajeLogin'></h1>
-
-<input type="text" id="nombre" placeholder="Ingrese nombre" class="correo">
-<input type="text" id="apellido" placeholder="Ingrese apellido" class="correo">
-<input type="text" id="fecha" placeholder="Ingrese fecha" class="correo"> <br>
-<button class="submit" id="btnGuardarPerfil">Guardar</button>
-
+<div class="sidebar">
+  <input type="checkbox" id="check">
+  <label for="check">
+    <span>&#9776</span>  
+  </label>
+  <nav class="sidebarNav">
+    <ul>
+      <li <a href="#/publicaciones">Perfil</a></li>
+      <li <a href="#/eventos">Eventos</a></li>
+      <li <a href="#/">Cerrar Sesión</a></li>
+    </ul>
+  </nav>
+</div>
+</header>
+<section class="container-columns">
+<br><br><br><br><br><br> 
+  <div class="column1">
+    <div class="profile-section">
+      <img id="profilePhoto" class="profile-picture" src="${userPhoto}" alt="">
+    </div>
+      <h2 class="user-name">${userName}</h2>
+  </div>
+  <div class="column2">
+  <textarea placeholder="Comparte tus ideas" id="comentario" cols="30" rows="10"></textarea>
+  <div class="container-button">
+    <input class='correo' type="file" id="imagen">
+    <img id="foto"/>
+    <button id="btnNewPost" class="btn-post">Publicar</button>
+  </div>
+  </div>
+  <div class="column3">
+    <h3>Eventos</h3>
+    <a href=https://mujeresemprendedoras.cl/web><img src="img/evento.jpg" alt=""></a>
+    <a href=https://avonni.cl><img src="img/evento2.jpg" alt=""></a>
+    <a href=https://laquintaemprende.cl/eventos/seminario-online-mujeres-emprendedoras><img src="img/evento3.jpg" alt=""></a>
+    <a href=https://laquintaemprende.cl/2020/04/01/comienza-de-forma-gratuita-y-online-el-viaje-de-la-emprendedora><img src="img/evento1.png" alt=""></a>
+  </div>
+</section>
 <div class="footer">
- <p>Copyright by Marcela / Paula & Yosliana / 2020 / Laboratoria013</p>
-</div> 
-
-
-`;
+  <p>Copyright by Marcela / Paula & Yosliana / 2020 / Laboratoria013</p>
+</div>
+</div>
+    
+    `;
   const divElemen = document.createElement('div');
   divElemen.innerHTML = muroEmprendedora;
-  const btnCerrarSesion = divElemen.querySelector('#cerrar');
-  btnCerrarSesion.addEventListener('click', cerrarSesion);
-
   return divElemen;
 };
